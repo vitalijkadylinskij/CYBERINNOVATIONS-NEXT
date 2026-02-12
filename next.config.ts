17cd55820e78e357960afa93e56be77b117f7cbb
Vitalii Kadylinskij <vitalijkadylinskij@gmail.com>
2026-02-12 13:34:15 +0300

    change config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/my-app",
-  assetPrefix: "/my-app",
   images: {
     unoptimized: true,
   },
